15c5e6ec4950f30d2dda996db6b1a2884c093a33
YannTomko <yann.tomkowiak@gmail.com>
2025-04-08 16:11:10 +0200

    exo3 a peine commencé

---
+++ exo3.py
@@ -0,0 +1,18 @@
+import unittest
+import random
+
+from io import StringIO
+from unittest.mock import patch
+
+class Game :
+    def __init__(self):
+        pass
+    def roll(self,pins)−>None:
+        pass
+    def score(self)−>int:
+        return random(0,10)
+
+def prog(n):
+
+
+class TestProg(unittest.TestCase):
